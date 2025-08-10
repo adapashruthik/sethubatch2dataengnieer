9c55c3c225709b1251ff666513e22c2e135197bb
TARUN BANALA <banalatarun1@gmail.com>
2025-08-10 19:29:58 +0530

    Create Tarun.Banala_089_July_30.py

---
+++ Tarun.Banala_089_July_30.py
@@ -0,0 +1,10 @@
+#Name: Tarun Banala   30-07-2025
+
+n=int(input("enter number of lines: "))
+for i in range(1,n+1):
+    for j in range(i+1,n+1):
+        print(" ",end="")
+    print("*"*i,end="")
+    for k in range(1,i):
+        print("*",end="")
+    print()
