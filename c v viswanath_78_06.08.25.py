a5d7ebe3e4d5646f3bc433bf904f2bcd583a6334
cvviswanath <viswanathvenkataram@gmail.com>
2025-08-10 21:29:13 +0530

    c v viswanath_78_06.08.25.py

---
+++ c v viswanath_78_06.08.25.py
@@ -0,0 +1,29 @@
+Modify  following   program  with  walrus  operator
+Hint:  Combine  lines  7   and   8  to a  single  line  with   walrus  operator
+try:
+	sum =  ctr = 0
+	while  True:
+		x = eval(input('Enter input  (ctrl + z  to  stop)  :  '))
+		sum += x
+		ctr +=1
+except  EOFError:
+	try:
+		print(F'Average :   {sum / ctr}')
+	except  ZeroDivisionError:
+		print('Enter  at  least  one  input')
+except  (NameError , TypeError):
+	print('Input  can  not  be  string')
+
+Code after modification
+try:
+	sum = ctr = 0
+	while True:
+		sum += (x := eval(input('Enter input  (ctrl + z  to  stop)  :  ')))
+		ctr += 1
+except EOFError:
+	try:
+		print(f'Average : {sum / ctr}')
+	except ZeroDivisionError:
+		print('Enter at least one input')
+except (NameError, TypeError):
+	print('Input can not be string')
