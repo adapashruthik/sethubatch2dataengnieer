7d65d6ccc120f5744f7c56fb325a80712d8329a1
adigopulajayakrishna <adigopulajayakrishna@gmail.com>
2025-08-10 21:36:05 +0530

    Add files via upload

---
+++ a.jayakrishna_102_15-7-25.py
@@ -0,0 +1,113 @@
+# float  object  demo  program (Home  work)
+a = 10.8
+print(a) # value of the object 'a' i.e, 10.8
+print(type(a)) # type of object 'a' i e, <class 'float'>
+print(id(a)) # address of object 'a' ex:- 1000
+b = 25.
+print(b) # value of the object 'b' i.e, 25.
+print(type(b)) # type of object 'b' i.e, <class 'int'>
+c = .689
+print(c) # value of the object 'c' i.e, 0.689
+d = 3.4E2
+print(d) # value of the object 'd' i.e, 3.4*10^2 = 340.0
+print(type(d)) # type of the object 'd' i.e, mantissa-exponential <class 'float' >
+e = 9.62e-2
+print(e) #value of the object 'e' i.e, 9.62*10^-2 = 0.0962
+#print(9.8.2) # error raised because of invalid syntax
+
+
+
+
+
+# complex object demo program
+a = 3 + 4j
+print(a) # value of the object 'a' i.e, (3+4j)
+print(type(a)) # type of the object 'a' i.e, <class 'complex'>
+print(id(a)) # address of the object 'a'  ex:- 2000
+print(a . real) # value of the real part object in 'a' i.e, 3.0
+print(a . imag) # value of the imaginary part object in 'a' i.e, 4.0
+print(type(a . real)) # type of the real part object in 'a' <class 'float'>
+print(type(a . imag)) # type of the imaginary part object in 'a' <class 'float'>
+
+
+
+
+# Find outputs (Home work)
+a = 6j
+print(a) # value of the object 'a' i.e, 6j
+print(type(a)) # type of the object 'a' i.e, < class 'complex' >
+print(a.real) # value of the real part object in 'a' i.e, 0.0
+print(a.imag) # value of the imaginary part object in 'a' i.e, 6.0
+#print(5 + j6) # error raised invalid sintax due to j is placed before the imag
+#print(3 + 4i)  # error raised because of there is no 'i' in complex type in python 
+#print(4+j) # error raised because of there is no imag before j
+print(4 + 1j) # (4 + 1j)
+print(4 + 0j) # (4 + 0j)
+
+
+
+
+# bool object demo program  (Home  work)
+a = True
+print(a) #value of the object 'a' i.e, True
+print(type(a)) #type of the object 'a' i.e, <class 'bool'>'''
+print(id(a)) #address of the object 'a' ex:- 3000'''
+b = False
+print(b) #value of the object 'b' i.e, False
+print(type(b)) #type of the object 'b' i.e, <class 'bool'>'''
+print(True + True) # 2 i.e, 1+1=2
+print(True + False) # 1 i.e, 1+0=1
+print(False + True) # 1 i.e, 0+1=1
+print(False + False) # 0 i.e, 0+0=0
+print(True + True + True) # 3 i.e, 1+1+1=3
+print(25 + 10.8 + True) # 36.8 i.e, 25+10.8+1=36.8
+print(True > False) # True i.e, 1>0
+print(True) # (True)
+print(False) # (False)
+#print(true) ''' error raised syntax error because of true is a keyword t should be capital like this 'T' '''
+#print(false) ''' error raised syntax error because of false is a keyword 'f' should be capital like this 'F' '''
+
+
+
+
+
+
+# Find  outputs (Home  work)
+a = 0O6247
+print(a) # value of the object 'a' i.e, 3223 
+print(type(a)) # type of the object 'a' i.e, <class 'int'>'''
+print(id(a)) # address of the object 'a' ex:- 100
+b = 0o6247
+print(id(b)) # address of the object 'b' ex:- 100
+print(b) # value of the object 'b' i.e, 3223
+c = 3239
+print(c) # value of the object 'c' i.e, 3239
+print(id(c)) # address of the object 'c' ex:- 500
+#print(0o9248) # error raised because of octal contains 0-7 numbers'''
+
+
+
+
+
+
+# Find  outputs  (Home  work)
+a = 0XA7B9
+print(a) #value of the object 'a' i.e, 42937 '''
+print(type(a)) # type of the object 'a' i.e, <class 'int'> '''
+b = 0xBEEF
+print(b) # value of the object 'b' i.e, 48879
+#print(A7B9) # invalid syntax 
+print('A7B9') # A7B9 i.e, it is a string.
+#print(0XBEER) #syntax error because there is no 'R' in hexa decimal '''
+#print(0XHYD) # syntax error because there is no 'H' 'Y' 'D' in hexa decimal '''
+#print(0xA7G9B) # syntax error because there is no 'G' in hexa decimal '''
+
+
+
+
+
+# Find outputs (Home  work)
+a = 9248
+print(a) # value of the object 'a' i.e, 9248
+print(type(a)) # type of the object 'a' i.e, <class 'int'>
+
