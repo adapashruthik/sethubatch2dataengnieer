b1a62746fbf2b72a61f99873fe7030a622ced9c5
Ramu81799 <ramuramu81799@gmail.com>
2025-08-11 09:42:52 +0530

    Create Ramu_SSSSDP-035_23-07-25.py

---
+++ Ramu_SSSSDP-035_23-07-25.py
@@ -0,0 +1,91 @@
+#  ++  and  --  operators  demo  program
+a = 25
+print(++a)  # 25 
+print(a++)  # Error 
+print(a++1)  # a=a+(+1)Error 
+print(--a)  # -25 
+print(a--)  # Error 
+print(a--1)  # Error 
+print(-a)  # -25 
+print(+-a)  # -25 
+print(-+a)  # -25
+
+#  Semicolon  demo  program
+print('One');  # One
+print('Two');  # Two
+print('Three');  # Three
+print('Hyd')  ;   print('Sec')  ;  print('Cyb')  
+
+#  floor()  and  ceil()  functions  demo  program
+import math
+print(math.floor(10.8))   # 10 
+print(math.ceil(10.8))    # 11 
+print(math.floor(25.0))   # 25
+print(math.ceil(25.0))    # 25
+print(math.floor(-3.5))   # -4 
+print(math.ceil(-3.5))    # -3 
+print(math.floor(-9.0))   # -9
+print(math.ceil(-9.0))    # -9
+print(math.floor(25.1))   # 25
+print(math.ceil(25.1))    # 26
+print(floor(3.5))         # Error 
+print(ceil(3.5))          #Error 
+
+# gcd()  function  demo  program
+import math
+print(math.gcd(12 , 15))     # 3 
+print(math.gcd(12 , 18))     # 6 
+print(math.gcd(4 , 7))       # 1 
+print(math.gcd(7 , 7))       # 7 
+print(math.gcd(-18 , -27))   # 9 
+print(math.gcd(-4 , 6))      # 2 
+print(math.gcd(0 , 7))       # 7 
+print(math.gcd(3 , 0))       # 3 
+print(math.gcd(0 , 0))       # 0 
+print(gcd(5 , 15))           #Error 
+
+#  abs()  function  demo  program
+from builtins import abs
+print(abs(-35.8))   # 35.8 
+print(abs(-27))     # 27
+print(abs(29.5))    # 29.5
+print(abs(32))      # 32
+import builtins
+print(builtins.abs(-25))  # 25 
+
+#  max()  and  min()   functions  demo  program
+from builtins import max, min
+print(max(10.8 , 20.6))  # 20.6 
+print(min(10.8 , 20.6 , 5.9 , 12.3))  # 5.9 
+print(max(25 , 10.8))  # 25 
+import builtins
+print(builtins.max(10 , 20 , 30))  # 30 
+print(builtins.min(10 , 20 , 15 , 5 , 12))  # 5 
+
+# pow()  function  demo  program
+from builtins import pow
+print(pow(10 , -2))         # 0.01 
+print(pow(4 , pow(3 , 2))) 
+import builtins
+print(builtins.pow(2 , 3))     # 8 
+print(builtins.pow(-2 , -3))   # -0.125 
+
+# Find  outputs
+# How to import kwlist
+import keyword 
+# How to print kwlist
+print(keyword.kwlist)  # prints list of all Python keywords
+print(len(keyword.kwlist)) # How to print number of keywords
+print(type(keyword.kwlist))  # <class 'list'># How to print type of kwlist
+print(keyword.kwlist)  # prints the keyword list again
+
+#  Find  outputs  (Home  work)
+# How to import keyword module
+import keyword
+# How to print kwlist
+print(keyword.kwlist)  # prints the list of all Python keywords
+# How to print number of keywords
+print(len(keyword.kwlist))  # prints total number of 
+# How to print type of kwlist
+print(type(keyword.kwlist))   <class 'list'>
+print(kwlist)  # Error
