3fab473a062cfd16717deb26a79496068f92ca28
Sirchan-cmd <sirigirichandra999@gmail.com>
2025-08-11 18:19:22 +0530

    schandra_109_28_07_25.py

---
+++ S chandra_109_28_07_25.py
@@ -0,0 +1,436 @@
+
+# Program to perform operations on two complex numbers
+
+a = 3 + 4j
+b = 5 + 6j
+
+print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)
+print("Division =", a / b)
+
+
+
+
+
+ # Identify  error
+else:
+		print('else  suite')
+print('Outside')
+
+### error: due to else without if
+
+
+: # Identify  error
+if  9 > 5
+	print('Hello')
+print('Bye')
+
+### error: due to missing colon: after if
+
+
+: # Identify  error
+if  9 > 12:
+	print('Hyd')
+else
+	print('Sec')
+
+### error: due to missing colon: after else 
+
+
+: # Identify  error
+if  (10,20,15):
+print('Hyd')
+else:
+print('Sec')
+
+### error: due to indentation error
+
+
+: # Identify  error
+if  ():
+			print('Hyd')
+	else:
+					print('Sec')
+print('Bye')
+
+### error:due to  empty tuple is false than condition if is not executed
+
+
+: # Identify  error
+if  { }:
+{
+	print('One')
+	print('Two')
+	print('Three')
+}
+else:
+{
+	print('Four')
+	print('Five')
+	print('Six')
+}
+print('Bye')
+
+### error: python does not use {}
+
+
+: # Identify  error
+if  ():
+	print('One')
+	print('Two')
+	print('Three')
+else:
+if  []:
+	print('Four')
+	print('Five')
+	print('Six')
+else:
+if  {}:
+	print('Seven')
+	print('Eight')
+	print('Nine')
+else:
+	print('Hyd')
+	print('Sec')
+	print('Cyb')
+print('Bye')
+
+### error: due to  nested if without elif or proper indentation
+
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+: # Find outputs  (Home  work)
+if():
+        print('Hyd')
+        print('Sec')
+        print('Cyb')
+else:
+        print('One')
+        print('Two')
+        print('Three')
+print('Bye') 
+
+output: One
+        Two
+        Three
+        Bye 
+due to condition if tuple is false else condition is executed
+
+: # Find  outputs  (Home  work)
+if{10 : 20 , 30 : 40}:
+        print('Hyd')
+        print('Sec')
+        print('Cyb')
+print('Bye')
+
+output:Hyd
+       Sec
+       Cyb
+       Bye
+
+: # Find outputs  (Home  work)
+if { }:
+	print('Hyd')
+	print('Sec')
+	print('Cyb')
+print('Bye')
+
+### empty dictonary is false output is Bye
+
+# Write   a  program  to  print  month  name  for  input  month  number  by  using  if  and  elif  (Home  work)
+
+month = input("Enter month number (1 - 12): ")
+
+if month.isdigit():
+    m = int(month)
+    if m == 1:
+        print("January")
+    elif m == 2:
+        print("February")
+    elif m == 3:
+        print("March")
+    elif m == 4:
+        print("April")
+    elif m == 5:
+        print("May")
+    elif m == 6:
+        print("June")
+    elif m == 7:
+        print("July")
+    elif m == 8:
+        print("August")
+    elif m == 9:
+        print("September")
+    elif m == 10:
+        print("October")
+    elif m == 11:
+        print("November")
+    elif m == 12:
+        print("December")
+    else:
+        print("Input should be between 1 and 12")
+else:
+    print("Input should be an integer") 
+
+digit = input("Enter a digit (0 - 9): ")
+
+if digit == '0':
+    print("Zero")
+else:
+    if digit == '1':
+        print("One")
+    else:
+        if digit == '2':
+            print("Two")
+        else:
+            if digit == '3':
+                print("Three")
+            else:
+                if digit == '4':
+                    print("Four")
+                else:
+                    if digit == '5':
+                        print("Five")
+                    else:
+                        if digit == '6':
+                            print("Six")
+                        else:
+                            if digit == '7':
+                                print("Seven")
+                            else:
+                                if digit == '8':
+                                    print("Eight")
+                                else:
+                                    if digit == '9':
+                                        print("Nine")
+                                    else:
+                                        print("Invalid")
+
+
+
+
+
+s. Chandra sekhar (home work)
+
+
+
+# Program to perform operations on two complex numbers
+
+a = 3 + 4j
+b = 5 + 6j
+
+print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)
+print("Division =", a / b)
+
+
+
+
+
+ # Identify  error
+else:
+		print('else  suite')
+print('Outside')
+
+### error: due to else without if
+
+
+: # Identify  error
+if  9 > 5
+	print('Hello')
+print('Bye')
+
+### error: due to missing colon: after if
+
+
+: # Identify  error
+if  9 > 12:
+	print('Hyd')
+else
+	print('Sec')
+
+### error: due to missing colon: after else 
+
+
+: # Identify  error
+if  (10,20,15):
+print('Hyd')
+else:
+print('Sec')
+
+### error: due to indentation error
+
+
+: # Identify  error
+if  ():
+			print('Hyd')
+	else:
+					print('Sec')
+print('Bye')
+
+### error:due to  empty tuple is false than condition if is not executed
+
+
+: # Identify  error
+if  { }:
+{
+	print('One')
+	print('Two')
+	print('Three')
+}
+else:
+{
+	print('Four')
+	print('Five')
+	print('Six')
+}
+print('Bye')
+
+### error: python does not use {}
+
+
+: # Identify  error
+if  ():
+	print('One')
+	print('Two')
+	print('Three')
+else:
+if  []:
+	print('Four')
+	print('Five')
+	print('Six')
+else:
+if  {}:
+	print('Seven')
+	print('Eight')
+	print('Nine')
+else:
+	print('Hyd')
+	print('Sec')
+	print('Cyb')
+print('Bye')
+
+### error: due to  nested if without elif or proper indentation
+
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+: # Find outputs  (Home  work)
+if():
+        print('Hyd')
+        print('Sec')
+        print('Cyb')
+else:
+        print('One')
+        print('Two')
+        print('Three')
+print('Bye') 
+
+output: One
+        Two
+        Three
+        Bye 
+due to condition if tuple is false else condition is executed
+
+: # Find  outputs  (Home  work)
+if{10 : 20 , 30 : 40}:
+        print('Hyd')
+        print('Sec')
+        print('Cyb')
+print('Bye')
+
+output:Hyd
+       Sec
+       Cyb
+       Bye
+
+: # Find outputs  (Home  work)
+if { }:
+	print('Hyd')
+	print('Sec')
+	print('Cyb')
+print('Bye')
+
+### empty dictonary is false output is Bye
+
+# Write   a  program  to  print  month  name  for  input  month  number  by  using  if  and  elif  (Home  work)
+
+month = input("Enter month number (1 - 12): ")
+
+if month.isdigit():
+    m = int(month)
+    if m == 1:
+        print("January")
+    elif m == 2:
+        print("February")
+    elif m == 3:
+        print("March")
+    elif m == 4:
+        print("April")
+    elif m == 5:
+        print("May")
+    elif m == 6:
+        print("June")
+    elif m == 7:
+        print("July")
+    elif m == 8:
+        print("August")
+    elif m == 9:
+        print("September")
+    elif m == 10:
+        print("October")
+    elif m == 11:
+        print("November")
+    elif m == 12:
+        print("December")
+    else:
+        print("Input should be between 1 and 12")
+else:
+    print("Input should be an integer") 
+
+digit = input("Enter a digit (0 - 9): ")
+
+if digit == '0':
+    print("Zero")
+else:
+    if digit == '1':
+        print("One")
+    else:
+        if digit == '2':
+            print("Two")
+        else:
+            if digit == '3':
+                print("Three")
+            else:
+                if digit == '4':
+                    print("Four")
+                else:
+                    if digit == '5':
+                        print("Five")
+                    else:
+                        if digit == '6':
+                            print("Six")
+                        else:
+                            if digit == '7':
+                                print("Seven")
+                            else:
+                                if digit == '8':
+                                    print("Eight")
+                                else:
+                                    if digit == '9':
+                                        print("Nine")
+                                    else:
+                                        print("Invalid")
