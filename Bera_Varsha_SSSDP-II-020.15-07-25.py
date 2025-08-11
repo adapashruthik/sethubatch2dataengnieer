50e544ad6c55bec1629f805065f06c7b8d800446
varshabera <varshabera529@gmail.com>
2025-08-11 14:27:07 +0530

    Add files via upload

---
+++ Bera_Varsha_SSSDP-II-020.15-07-25.py
@@ -0,0 +1,160 @@
+# float  object  demo  program (Home  work)
+a = 10.8
+print(a)
+print(type(a))
+print(id(a))
+b = 25.
+print(b)
+print(type(b))
+c = .689
+print(c)
+d = 3.4E2
+print(d)
+print(type(d))
+e = 9.62e-2
+print(e)
+print(9.8.2)
+"""
+output:
+10.8
+<class 'float'>
+2321571005200
+25.0
+<class 'float'>
+0.689
+340.0
+<class 'float'>
+0.0962
+#print(9.8.2)Error due to more than one decimal point
+"""
+# complex object demo program
+a = 3 + 4j
+print(a)
+print(type(a))
+print(id(a))
+print(a . real)
+print(a . imag)
+print(type(a . real))
+print(type(a . imag))
+"""
+(3+4j)
+<class 'complex'>
+2097376021552
+3.0
+4.0
+<class 'float'>
+<class 'float'>
+"""
+# Find outputs (Home work)
+a = 6j
+print(a)
+print(type(a))
+print(a.real)
+print(a.imag)
+print(5 + j6)
+print(3 + 4i)  
+print(4+j)
+print(4 + 1j)
+print(4 + 0j)
+"""
+output:
+6j
+<class 'complex'>
+0.0
+6.0
+error : img can not be after j
+error due to 'i' at 4i
+  print(4+j)
+            ^
+NameError: name 'j' is not defined
+4+1j
+4+0j
+"""
+# bool object demo program  (Home  work)
+a = True
+print(a)
+print(type(a))
+print(id(a))
+b = False
+print(b)
+print(type(b))
+print(True + True)
+print(True + False)
+print(False + True)
+print(False + False)
+print(True + True + True)
+print(25 + 10.8 + True)
+print(True > False)
+print(True)
+print(False)
+print(true)
+print(false)
+"""
+True
+<class 'bool'>
+140737378327464
+False
+<class 'bool'>
+2
+1
+1
+0
+3
+36.8
+True
+True
+False
+#Error due to 't'
+#Error due to 'f'
+"""
+# Find  outputs (Home  work)
+a = 0O6247
+print(a)
+print(type(a))
+print(id(a))
+b = 0o6247
+print(id(b))
+print(b)
+c = 3239
+print(c)
+print(id(c))
+print(0o9248)
+"""
+3239
+<class 'int'>
+2324242787504
+2324242787504
+3239
+3239
+2324242787504
+error due to 9 and 8 in octal number
+"""
+# Find  outputs  (Home  work)
+a = 0XA7B9
+print(a)
+print(type(a))
+b = 0xBEEF
+print(b)
+print(A7B9)
+print('A7B9')
+print(0XBEER)
+print(0XHYD)
+print(0xA7G9B)
+# """
+# 42937
+# <class 'int'>
+# 48879
+# print(A7B9)error not starting with ox
+# A7B9
+# print(0XBEER)error due to 'R' in hexadecimal number
+# print(0XHYD)error due to  'H' and'Y' in hexadecimal number
+# print(0xA7G9B)error due to 'G' in hexa-decimal number
+# """
+# Find outputs (Home  work)
+a = 9248
+print(a)
+print(type(a))
+"""
+9248
+<class 'int'>
+"""
