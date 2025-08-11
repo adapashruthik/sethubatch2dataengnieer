3427f7d47a8a47efa36bc537297535990ac46228
Sirchan-cmd <sirigirichandra999@gmail.com>
2025-08-11 17:24:43 +0530

    SChandra_109_15_7_25.py

---
+++ SChandra_109_15_7_25.py
@@ -0,0 +1,164 @@
+float  object  demo  program (Home  work)
+a = 10.8
+print(a)
+Output:10.8
+print(type(a))
+Output:   class float
+print(id(a))
+Output: adress of object 'a'
+b = 25.
+print(b)
+Output:25.0
+print(type(b))
+Output:class float
+c = .689
+print(c)
+Output:0.689
+d = 3.4E2
+print(d)
+Output:3.4*10^2=340.0
+
+print(type(d))
+Output:class float
+e = 9.62e-2
+print(e)
+Output:0.0962
+print(9.8.2)
+Output:error if we place coma output will occur other wise it is invalid syntax
+ # complex object demo program
+a = 3 + 4j
+print(a)
+Output:3+4j
+print(type(a))
+Output: class complex 
+print(id(a))
+Output: adress of object a
+print(a . real)
+Output:3.0
+print(a . imag)
+Output:4.0
+print(type(a . real))
+Output: class float
+print(type(a . imag))
+Output:class float
+ # Find outputs (Home work)
+a = 6j
+print(a)
+Output:6j
+
+print(type(a))
+Output:class complex 
+print(a.real)
+Output:0.0
+print(a.imag)
+Output:6. 0
+print(5 + j6)
+Output:5+j6
+print(3 + 4i)  
+Output: error due to invalid decimal literal
+print(4+j)
+Output:j is not defined
+
+print(4 + 1j)
+Output:4.0
+
+print(4 + 0j)
+Output:4.0
+
+[# bool object demo program  (Home  work)
+a = True
+print(a)
+Output:true
+
+print(type(a))
+Output:class boolean
+
+print(id(a))
+Output:adress of an object a
+
+b = False
+print(b)
+Output:False
+
+print(type(b))
+Output:class boolean
+
+print(True + True)
+Output:2
+
+print(True + False)
+Output:1
+
+print(False + True)
+Output:1
+
+print(False + False)
+Output:0
+
+print(True + True + True)
+Output:
+3
+print(25 + 10.8 + True)
+Output:36.8
+
+print(True > False)
+Output:True
+
+print(True)
+Output:True
+
+print(False)
+Output:False
+
+print(true)
+Output: true is not defined
+
+print(false)
+Output:error  false is not defined 
+
+[ # Find  outputs (Home  work)
+a = 0O6247
+print(a)
+Output:6 * 512 + 2 * 64 + 4 * 8 + 7 * 1 = 3239
+print(type(a))
+Outpu:class int
+print(id(a))
+Output:adress of an object 'a'
+b = 0o6247
+print(id(b))
+Same adress of an object a
+print(b)
+Output:3239 same output of object a
+c = 3239
+print(c)
+Output:3239
+print(id(c))
+Output:same adress of object a
+print(0o9248)
+
+# Find  outputs  (Home  work)
+a = 0XA7B9
+print(a)
+Output :    4096*10 +256*7+11*16+1*9=42937
+print(type(a))
+Output:class int
+b = 0xBEEF
+print(b)
+Output: 4096*11+14*256+14*16+15*1=48879
+print(A7B9)
+Output :error A7B9 IS NOT DEFINED 
+
+print('A7B9')
+OUTPUT:A7B9
+print(0XBEER)
+Output : error because Oxbeer is not defined 
+print(0XHYD)
+Output:error
+print(0xA7G9B)
+Output :error
+ # Find outputs (Home  work)
+a = 9248
+print(a)
+Output:9248
+print(type(a))
+Output: Class int
