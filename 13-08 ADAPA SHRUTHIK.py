26a56151ba8eda37a81c3d9c861cd20fcc737e23
adapashruthik <167800499+adapashruthik@users.noreply.github.com>
2025-08-14 09:15:56 +0530

    Add files via upload

---
+++ 13-08 ADAPA SHRUTHIK.py
@@ -0,0 +1,19 @@
+'''
+Write  a  program  to  print  distinct  vowels  of  the  string  without  using  set
+
+1) Let  input  be   RaMA  rAo
+    What  is  the  output ?  ---> AO
+
+2) Hint  1:  Same  as   prog3e  with  minor  changes
+
+3) What  does  'hyd' . upper()  do ? --->  Returns  'HYD'
+'''
+
+s=input().upper()
+v='AEIOU'
+res=''
+for i in s:
+  if i in v and i not in res:
+    res+=i
+  
+print(res)
